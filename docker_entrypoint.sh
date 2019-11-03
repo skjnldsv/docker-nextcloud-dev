39ae97611e70f479c2bb6796d8ca33dd14a7a09e
John Molakvoæ (skjnldsv) <skjnldsv@protonmail.com>
2019-11-03 15:34:48 +0100

    Add host ip to hosts
    Signed-off-by: John Molakvoæ (skjnldsv) <skjnldsv@protonmail.com>

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -1,3 +1,3 @@
 # !/bin/env sh
-local_ip = ip -4 route list match 0/0 | awk '{print $3 "\thost.docker.internal"}'
-echo $local_ip
+# https://github.com/docker/for-linux/issues/264
+ip -4 route list match 0/0 | awk '{print $3 "\thost.docker.internal"}' >> /etc/hosts
